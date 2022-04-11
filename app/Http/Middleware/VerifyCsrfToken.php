8ac0100f005af00759ab36a8f23e891377fba21c
treepo1 <78393422+treepo1@users.noreply.github.com>
2022-04-10 23:06:18 -0300

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,9 +12,9 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'http://127.0.0.1:8000/api/user/',
-        'http://127.0.0.1:8000/api/user/*',
-        'http://127.0.0.1:8000/api/login'
+        'https://desafio-back-php.herokuapp.com/api/user/',
+        'https://desafio-back-php.herokuapp.com/api/user/*',
+        'https://desafio-back-php.herokuapp.com/api/login'
 
 
         //
